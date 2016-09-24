2f3deaa60b83ed758d9ccac60838f1b4992e9970
Orion10110 <pojasoriona@gmail.com>
2016-09-24 17:54:00 +0300

    Ui interfase
    UI

--- src/student/logic/ManagementSystem.java
+++ src/student/logic/ManagementSystem.java
@@ -30,127 +30,7 @@ public class ManagementSystem {
 	        return instance;
 	    }
 	 
-	 public static void main(String[] args) {
-	      
-	        try {
-	            System.setOut(new PrintStream("out.txt"));
-	        } catch (FileNotFoundException ex) {
-	            ex.printStackTrace();
-	            return;
-	        }
-	 
-	        ManagementSystem ms = ManagementSystem.getInstance();
-	 
-	       
-	        printString("Полный список групп");
-	        printString("*******************");
-	        List<Group> allGroups = ms.getGroups();
-	        for (Group gi : allGroups) {
-	            printString(gi);
-	        }
-	        printString();
-	 
-	       
-	        printString("Полный список студентов");
-	        printString("***********************");
-	        Collection<Student> allStudends = ms.getAllStudents();
-	        for (Student si : allStudends) {
-	            printString(si);
-	        }
-	        printString();
-	 
-	     
-	        printString("Список студентов по группам");
-	        printString("***************************");
-	        List<Group> groups = ms.getGroups();
-	    
-	        for (Group gi : groups) {
-	            printString("---> Группа:" + gi.getNameGroup());
-	            Collection<Student> students = ms.getStudentsFromGroup(gi, 2006);
-	            for (Student si : students) {
-	                printString(si);
-	            }
-	        }
-	        printString();
-	 
-	       
-	        Student s = new Student();
-	        s.setStudentId(5);
-	        s.setFirstName("Игорь");
-	        s.setPatronymic("Владимирович");
-	        s.setSurName("Перебежкин");
-	        s.setSex('М');
-	        Calendar c = Calendar.getInstance();
-	        c.set(1991, 8, 31);
-	        s.setDateOfBirth(c.getTime());
-	        s.setGroupId(1);
-	        s.setEducationYear(2006);
-	        printString("Добавление студента:" + s);
-	        printString("********************");
-	        ms.insertStudent(s);
-	        printString("--->> Полный список студентов после добавления");
-	        allStudends = ms.getAllStudents();
-	        for (Student si : allStudends) {
-	            printString(si);
-	        }
-	        printString();
-	 
-	        s = new Student();
-	        s.setStudentId(5);
-	        s.setFirstName("Игорь");
-	        s.setPatronymic("Владимирович");
-	        s.setSurName("Новоперебежкин");
-	        s.setSex('М');
-	        c = Calendar.getInstance();
-	        c.set(1991, 8, 31);
-	        s.setDateOfBirth(c.getTime());
-	        s.setGroupId(1);
-	        s.setEducationYear(2006);
-	        printString("Редактирование данных студента:" + s);
-	        printString("*******************************");
-	        ms.updateStudent(s);
-	        printString("--->> Полный список студентов после редактирования");
-	        allStudends = ms.getAllStudents();
-	        for (Student si : allStudends) {
-	            printString(si);
-	        }
-	        printString();
-	 
-	        
-	        printString("Удаление студента:" + s);
-	        printString("******************");
-	        ms.deleteStudent(s);
-	        printString("--->> Полный список студентов после удаления");
-	        allStudends = ms.getAllStudents();
-	        for (Student si : allStudends) {
-	            printString(si);
-	        }
-	        printString();
-	 
-	          Group g1 = groups.get(0);
-	        Group g2 = groups.get(1);
-	        printString("Перевод студентов из 1-ой во 2-ю группу");
-	        printString("***************************************");
-	        ms.moveStudentsToGroup(g1, 2006, g2, 2007);
-	        printString("--->> Полный список студентов после перевода");
-	        allStudends = ms.getAllStudents();
-	        for (Student si : allStudends) {
-	            printString(si);
-	        }
-	        printString();
-	 
-	        printString("Удаление студентов из группы:" + g2 + " в 2006 году");
-	        printString("*****************************");
-	        ms.removeStudentsFromGroup(g2, 2006);
-	        printString("--->> Полный список студентов после удаления");
-	        allStudends = ms.getAllStudents();
-	        for (Iterator i = allStudends.iterator(); i.hasNext();) {
-	            printString(i.next());
-	        }
-	        printString();
-	    }
-	 
-	    public void loadGroups() {
+	 public void loadGroups() {
 	        if (groups == null) {
 	            groups = new ArrayList<Group>();
 	        } else {
@@ -303,11 +183,11 @@ public class ManagementSystem {
 	 
 	     public static void printString(Object s) {
 	        //System.out.println(s.toString());
-	        try {
-	            System.out.println(new String(s.toString().getBytes("windows-1251"), "windows-1252"));
-	        } catch (UnsupportedEncodingException ex) {
-	            ex.printStackTrace();
-	        }
+	      //  try { .getBytes("windows-1251"), "windows-1252")
+	            System.out.println(new String(s.toString()));
+	     //   } catch (UnsupportedEncodingException ex) {
+	     //       ex.printStackTrace();
+	     //   }
 	    }
 	 
 	    public static void printString() {
